bd24cb797ed7d5a17f4fb23923eb9cda1ea2bd10
Elsium <li.dsen@students.dvfu.ru>
2022-04-23 17:42:11 +1000

    Add first project

---
+++ samurai_first/src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+        <ul>
+            <li>asd</li>
+            <li>saf</li>
+        </ul>
+    </div>
+  );
+}
+
+export default App;
