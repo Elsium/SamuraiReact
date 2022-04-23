fbaca816fa461e5fbb74d7730d97ab78a3688e57
Elsium <li.dsen@students.dvfu.ru>
2022-04-23 18:25:06 +1000

    First division on components

--- samurai_first/src/App.js
+++ samurai_first/src/App.js
@@ -2,14 +2,34 @@ import React, {Component} from 'react';
 import './App.css';
 
 const App = () => {
-  return (
-    <div className="App">
-        <ul>
-            <li>asd</li>
-            <li>saf</li>
-        </ul>
-    </div>
-  );
+	return (
+	<div>
+		<Headerr />
+		<AnyComps />
+	</div>
+);
+}
+
+const Headerr = () => {
+	return (
+		<div>
+			<a href="#">home </a>
+			<a href="#">news </a>
+			<a href="#">mes</a>
+		</div>
+	);
+}
+
+const AnyComps = () => {
+	return (
+		<div>
+			<div>new div</div>
+			<ul>
+				<li>asd</li>
+				<li>saf</li>
+			</ul>
+		</div>
+	);
 }
 
 export default App;
