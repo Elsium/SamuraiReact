ce18b07acae73a40dd7685b774d17e436f540b68
Elsium <li.dsen@students.dvfu.ru>
2022-04-24 15:16:57 +1000

    Return 2 components to App.js and comment

--- samurai_first/src/App.js
+++ samurai_first/src/App.js
@@ -8,8 +8,32 @@ const App = () => {
 	<div>
 		<An />
 		<Hee />
+		{/*<Headerr />*/}
+		{/*<AnyComps />*/}
 	</div>
 );
 }
+
+// const Headerr = () => {
+// 	return (
+// 		<div>
+// 			<a href="#">home </a>
+// 			<a href="#">news </a>
+// 			<a href="#">mes</a>
+// 		</div>
+// 	);
+// }
+
+// const AnyComps = () => {
+// 	return (
+// 		<div>
+// 			<div>new div</div>
+// 			<ul>
+// 				<li>asd</li>
+// 				<li>saf</li>
+// 			</ul>
+// 		</div>
+// 	);
+// }
 
 export default App;
