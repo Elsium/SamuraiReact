fd6db1200cacbf66e195a81ab6851152754759ee
Elsium <li.dsen@students.dvfu.ru>
2022-04-24 15:13:26 +1000

    Add some components in files and redacted App.js: remove 2 components

---
+++ samurai_first/src/AnyComps.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+
+const AnyComps = () => {
+	return (
+		<div>
+			<div>new div</div>
+			<ul>
+				<li>asd</li>
+				<li>saf</li>
+			</ul>
+		</div>
+	);
+}
+
+export default AnyComps;
--- samurai_first/src/App.js
+++ samurai_first/src/App.js
@@ -1,35 +1,15 @@
 import React, {Component} from 'react';
 import './App.css';
+import An from './AnyComps'
+import Hee from './He'
 
 const App = () => {
 	return (
 	<div>
-		<Headerr />
-		<AnyComps />
+		<An />
+		<Hee />
 	</div>
 );
-}
-
-const Headerr = () => {
-	return (
-		<div>
-			<a href="#">home </a>
-			<a href="#">news </a>
-			<a href="#">mes</a>
-		</div>
-	);
-}
-
-const AnyComps = () => {
-	return (
-		<div>
-			<div>new div</div>
-			<ul>
-				<li>asd</li>
-				<li>saf</li>
-			</ul>
-		</div>
-	);
 }
 
 export default App;
